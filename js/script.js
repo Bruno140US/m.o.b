a64f8440b91b9badc6f80694b1c069c111804718
Bruno Rocha Garcia <bruno.rochag140@hotmail.com>
2023-03-19 12:23:59 -0400

    upd
    JS updated to adjust screen

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ menuIcon.onclick = () => {
 
 /*============================ scroll section active link ============================  */
 
-let sections = document.querySelectorAll('section')
-let navLinks = document.querySelectorAll('header nav a')
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-windown.onscroll = () => {
+window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
@@ -22,7 +22,7 @@ windown.onscroll = () => {
         if(top >= offset && top < offset + height ) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
             })
         };
     })
@@ -34,20 +34,19 @@ windown.onscroll = () => {
     /*============================ remove toggle icon and navbar when click navbar link (scroll) ============================*/
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
-
 }
 
 /*============================ scroll reveal ============================*/
-ScrollReveal({
+const scrollReveal = new ScrollReveal({
     distance: '80px',
     duration: 2000,
     delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .albums-container, platforms-container, albums-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+scrollReveal.reveal('.home-content, .heading', { origin: 'top' });
+scrollReveal.reveal('.home-img, .albums-container, .platforms-container, .albums-box, .contact form', { origin: 'bottom' });
+scrollReveal.reveal('.home-content h1, .about-img', { origin: 'left' });
+scrollReveal.reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*============================ type js ============================*/
 const typed = new Typed('.multiple-text', {
